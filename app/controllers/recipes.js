ce36f78437cb1579de536e43e563a120e61fb2b7
frank06 <me@frank06.net>
2016-10-19 23:12:31 -0300

    add ember-concurrency example

--- app/controllers/recipes.js
+++ app/controllers/recipes.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 const { computed, observer } = Ember;
 import DS from 'ember-data';
 import computedFilterByQuery from 'ember-cli-filter-by-query';
+import { task } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
 
@@ -9,24 +10,41 @@ export default Ember.Controller.extend({
 
   _filter1: observer('ingredients.@each.vegetarian', function() {
     this.get('ingredients').then(ingredients => {
-      this.set('filter1', ingredients.filter(i => i.get('vegetarian')));
+      this.set('filter1', ingredients.filterBy('vegetarian'));
     });
   }),
 
   filter2: computed('ingredients.@each.vegetarian', function() {
     return this.get('ingredients').then(ingredients => {
-      return ingredients.filter(i => i.get('vegetarian'));
+      return ingredients.filterBy('vegetarian');
     });
   }),
 
   filter3: computed('ingredients.@each.vegetarian', function() {
     return DS.PromiseArray.create({
       promise: this.get('ingredients').then(ingredients => {
-        return ingredients.filter(i => i.get('vegetarian'));
+        return ingredients.filterBy('vegetarian');
       })
     });
   }),
 
-  filter4: computedFilterByQuery('ingredients', 'name', 'input')
+  filter4: computed('ingredients.@each.vegetarian', function() {
+    return this.task1.perform();
+  }),
+
+  task1: task(function * (reload = false) {
+    const ingredients = yield this.get('ingredients').reload();
+    return ingredients.filterBy('vegetarian');
+  }).keepLatest(),
+
+  filter5: computedFilterByQuery('ingredients', 'name', 'input'),
+
+  actions: {
+
+    reload() {
+      this.notifyPropertyChange('ingredients.@each.vegetarian');
+    }
+
+  }
 
 });
